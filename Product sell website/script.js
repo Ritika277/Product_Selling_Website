fa8e3e5128fba6b040e90432ed339592363fb36b
Ritika277 <131886644+Ritika277@users.noreply.github.com>
2023-12-04 21:46:56 +0530

    Add files via upload

---
+++ Product sell website/script.js
@@ -0,0 +1,7 @@
+// Get the button element
+var btn = document.querySelector('.btn');
+
+// Add a click event listener to the button
+btn.addEventListener('click', function() {
+	alert('You clicked the button!');
+});
